a534b49808b97ca576276c698a96601846bd66f8
Maxonychh <savvateev.m32@gmail.com>
2021-04-28 10:54:51 +0300

    EDkat

---
+++ edkat/js/form.js
@@ -0,0 +1,21 @@
+//FORM
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Ваше сообщение успешно отправлено! Вы получите ответ в ближайшее время");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
---
+++ edkat/js/script.js
@@ -0,0 +1,32 @@
+// Preloader //
+
+jQuery(document).ready(function($) {  
+
+$(window).load(function(){
+  $('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+});
+
+var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+
+    var hamburgers = document.querySelectorAll(".hamburger, .main__menu");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+    }
+   
+// Closes the sidebar menu
+$("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// menu
+$(".hamburger").click(function(e) {
+    e.preventDefault();
+    $(".main__menu").toggleClass("main__menu__open");
+});
